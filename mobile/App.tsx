d95d23d192a010d60e41cff9959689e4c0665eb3
julia <agramartinsj@gmail.com>
2023-05-24 17:12:12 -0300

    initial commit

---
+++ mobile/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-950">
+      <Text className="text-5xl text-white">Oir</Text>
+      <StatusBar style="light" translucent />
+    </View>
+  )
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
