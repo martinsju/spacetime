c2a8cf7242f7818d4810481db8fcb9cc36a210f4
julia <agramartinsj@gmail.com>
2023-05-29 15:59:17 -0300

    refactor: create components

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Copyright() {
+  return (
+    <div className='text-sm leading-relaxed text-gray-200'>
+      Feito com 💜 no NLW da
+      <a
+        target='_blank'
+        rel='noreferrer'
+        href='https://rocketseat.com.br'
+        className='underline hover:text-gray-100'
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
+
+export default Copyright
